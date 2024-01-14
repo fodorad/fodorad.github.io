db419282353532e350fe971b10a36522a8b00f12
fodorad <fodorad201@gmail.com>
2024-01-14 23:35:32 +0100

    Add gallery pt.2

--- assets/js/event.loader.js
+++ assets/js/event.loader.js
@@ -6,11 +6,16 @@ fetch('content/events.json')
             const eventBox = document.createElement('article');
             eventBox.classList.add('event-box');
 
+            const galleryTagsLink = item.gallery_tags
+                ? `gallery.html?${item.gallery_tags.map(tag => `tag=${tag}`).join('&')}`
+                : '';
+
             eventBox.innerHTML =
                 `<img src="${item.image}" alt="${item.alt}">
                  <h3>${item.title}</h3>
                  <h4>${item.date}</h4>
-                 ${item.content.split('\n').map(sentence => `<p>${sentence}</p>`).join('')}`;
+                 ${item.content.split('\n').map(sentence => `<p>${sentence}</p>`).join('')}
+                 ${galleryTagsLink ? `<p><a href="${galleryTagsLink}">[gallery]</a></p>` : ''}`;
 
             eventContainer.appendChild(eventBox);
         });
