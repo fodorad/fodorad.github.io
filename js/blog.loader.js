34927bbd5795d42135b724b1a7ddd1ecfa0fb341
fodorad <fodorad201@gmail.com>
2024-04-05 13:34:52 +0200

    Add 'soon' for blog posts and fix gallery url filter for events

--- js/blog.loader.js
+++ js/blog.loader.js
@@ -9,10 +9,13 @@ fetch('../content/blog_posts.json')
             const blogPost = document.createElement('article');
             blogPost.classList.add('blog-post');
             blogPost.setAttribute('data-tags', item['data-tags']);
+
+            const imageHTML = item.image.endsWith('soon.jpg') 
+                ? `<img src="../${item.image}" alt="${item.alt}">`
+                : `<a href="${item.page}"><img src="../${item.image}" alt="${item.alt}"></a>`;
+
             blogPost.innerHTML =
-                `<a href="${item.page}">
-                    <img src="../${item.image}" alt="${item.alt}">
-                </a>
+                `${imageHTML}
                 <ul>
                     ${item['data-tags'].split(' ').map(tag => `<li class="tag" data-filter="${tag}">${tag}</li>`).join('')}
                 </ul>
